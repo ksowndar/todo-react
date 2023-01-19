b6ab7314d54a2ded91ff58f7aaadfcafba7d1adf
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-01-20 00:06:28 +0530

    Todo react is created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
 
+//Importing Components
+import Form from "./components/Form";
+import TodoList from './components/TodoList';
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  useEffect(() => {
+    const todosString = localStorage.getItem("todos");
+    if (todosString) {
+        const todos = JSON.parse(todosString);
+        setTodos([ 
+          ...todos, {text: inputText, completed: false, id:Math.random() * 1000 },
+        ]);
+    }
+
+    function storageEventHandler(event) {
+        if (event.key === "todos") {
+            const todos = JSON.parse(event.newValue);
+            setTodos([ 
+              ...todos, {text: inputText, completed: false, id:Math.random() * 1000 },
+            ]);
+        }
+    }
+  
+    window.addEventListener("storage", storageEventHandler);
+    return () => {
+        window.removeEventListener("storage", storageEventHandler);
+    };
+}, []);
+  
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true ));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false ));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break; 
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>Sowndar's Todo List</h1>         
+      </header> 
+      <Form inputText = {inputText} 
+            todos = {todos} 
+            setTodos = {setTodos} 
+            setInputText = {setInputText} 
+            setStatus = {setStatus}
+      />
+      <TodoList  filteredTodos = {filteredTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
 }
